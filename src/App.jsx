757bae5e38d8760d592cf4a90d1a45ed8738256e
xiokevc <xiokevc@gmail.com>
2025-08-12 10:18:55 -0400

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-const App = () => {
-  return <h1>Hello world!</h1>;
-};
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxList from './components/MailboxList/MailboxList';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+
+function App() {
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = (formData) => {
+    const newBox = {
+      ...formData,
+      _id: mailboxes.length + 1,
+    };
+    setMailboxes([...mailboxes, newBox]);
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
